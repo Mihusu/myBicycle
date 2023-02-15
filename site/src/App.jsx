78b56502fe08455ad553be13bfef77d791c2d0f6
Abdallah Ziad Al Naif <Abdallahalnaif@hotmail.com>
2023-02-15 15:42:58 +0100

    Some minimal correction and changes
    A design example of how  "Min Cykel" homepage could look like.

--- site/src/App.jsx
+++ site/src/App.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { createBrowserRouter,
    RouterProvider,
     Navigate } from "react-router-dom"
@@ -30,6 +31,26 @@ function App() {
 
   return <RouterProvider router={router} />
     
+=======
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+  let col_val = 500;
+
+  return (
+    <div className="App">
+      <div className='container max-w-sm mx-auto p-2 flex flex-col -space-y-24 hover:space-y-2'>
+        {[...Array(4)].map((_, key) => {
+          return (
+            <div key={key} className={`w-full h-44 bg-blue-500 shadow-xl border rounded -py-20`}>1</div>
+          )
+        })}
+      </div>
+    </div>
+  )
+>>>>>>> Stashed changes
 }
 
 export default App
