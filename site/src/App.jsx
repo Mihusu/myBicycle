cd84017d5e7217b6450668d3ce88e7513e4bf488
Abdallah Ziad Al Naif <Abdallahalnaif@hotmail.com>
2023-02-17 10:17:47 +0100

    Some minimal correction and changes
    A design example of how  "Min Cykel" homepage could look like.

--- site/src/App.jsx
+++ site/src/App.jsx
@@ -1,37 +1,3 @@
-<<<<<<< Updated upstream
-import { createBrowserRouter,
-   RouterProvider,
-    Navigate } from "react-router-dom"
-
-// Non-auth pages
-import Login from "./pages/Login";
-import NotFound from "./pages/NotFound";
-import MineCykler from "./pages/MineCykler";
-
-import './App.css'
-
-function App() {
-  // router
-  const router = createBrowserRouter([
-    { // Homepage is left unused
-      path: "/",
-      element: <Navigate to={"/minecykler"} />,
-      errorElement: <NotFound />
-    },
-    {
-      path: "/login",
-      element: <Login />,
-      errorElement: <NotFound />,
-    },
-    {
-      path: "/minecykler",
-      element: <MineCykler />,
-      errorElement: <NotFound />,
-    }])
-
-  return <RouterProvider router={router} />
-    
-=======
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 
@@ -44,13 +10,12 @@ function App() {
       <div className='container max-w-sm mx-auto p-2 flex flex-col -space-y-24 hover:space-y-2'>
         {[...Array(4)].map((_, key) => {
           return (
-            <div key={key} className={`w-full h-44 bg-blue-500 shadow-xl border rounded -py-20`}>1</div>
+            <div key={key} className={`w-full h-44 bg-blue-500 shadow-xl border rounded p-2`}>1</div>
           )
         })}
       </div>
     </div>
   )
->>>>>>> Stashed changes
 }
 
 export default App
