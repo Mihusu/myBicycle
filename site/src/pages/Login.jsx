f30cae0be362ffed610367d3f42218f0619c409d
Mihusu <ming2200@hotmail.com>
2023-03-16 15:52:54 +0100

    Updates to login page

--- site/src/pages/Login.jsx
+++ site/src/pages/Login.jsx
@@ -13,18 +13,18 @@ function Login() {
         formState: { errors },
     } = useForm({
         defaultValues: {
-            email: "",
+            phoneNumber: "",
             password: "",
         },
     });
 
-    // Define state variables to store the email and password inputs
-    // email regex: ^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$
-    const [email, setEmail] = useState('');
+    // Define state variables to store the phoneNumber and password inputs
+    // phoneNumber regex: ^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$
+    const [phoneNumber, setPhoneNumber] = useState('');
     const [password, setPassword] = useState("");
 
     // Watches the 
-    const watchInputs = watch(["email", "password"]);
+    const watchInputs = watch(["phoneNumber", "password"]);
 
     // checks whether password matches verification
     function matchPassword(password, verify) {
@@ -45,8 +45,8 @@ function Login() {
     const onSubmit = (data) => {
         // Prevent the default browser behavior
         data.preventDefault();
-        // Do something with the email and password inputs
-        console.log(email, password);
+        // Do something with the phoneNumber and password inputs
+        console.log(phoneNumber, password);
 
         console.log(data);
         const requestOptions = {
@@ -60,32 +60,32 @@ function Login() {
             .catch((error) => setError(error.message));
 
         // Clear the inputs
-        setEmail("");
+        setPhoneNumber("");
         setPassword("");
     };
 
     return (
-        <div className="flex flex-col items-center justify-center py-64">
+        <div className="flex flex-col items-center justify-center py-56">
             <div className="bg-white rounded-lg shadow dark:bg-gray-800 md:w-auto">
                 {/* Use a form element to wrap the inputs and button */}
                 <form   
-                    className="rounded-lg bg-white py-8 shadow dark:bg-gray-800 sm:px-2 md:w-auto md:px-8 lg:px-20"
+                    className="rounded-lg bg-white py-8 shadow dark:bg-gray-800 sm:px-2 md:px-4 lg:px-10"
                     onSubmit={handleSubmit(onSubmit, onError)}
                 >
-                    {/* Use input elements for email and password */}
+                    {/* Use input elements for phoneNumber and password */}
                     <div className="pt-4 pb-2 font-light text-gray-800 dark:text-white px-8"
                         control={control}
                         rules={{ required: true }}>
-                        E-mail:
+                        Tlf nr:
                         <span className="required-dot text-red-500"> *</span>
                     </div>
                     <div className="px-8">
                         <input
                             type="text"
-                            placeholder="E-mail"
+                            placeholder="Telefonnummer"
                             className="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
+                            value={phoneNumber}
+                            onChange={(e) => setPhoneNumber(e.target.value)}
                         />
                         {errors.password && <span>This field is required</span>}
                     </div>
