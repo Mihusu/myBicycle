10ed34aa354590b5e7e05ac2ff87ed786710e1c1
Abdallah Ziad Al Naif <Abdallahalnaif@hotmail.com>
2023-03-16 15:43:24 +0100

    Update BikeTransfer.jsx

--- site/src/pages/BikeTransfer.jsx
+++ site/src/pages/BikeTransfer.jsx
@@ -2,15 +2,7 @@ import React from "react";
 import { SwipeButton } from "../components/MyBikes/SwipeButton";
 
 const BikeTransfer = () => {
-  return( 
-  
-  <p>hhelloo</p>
-  
-<div>
- 
-<SwipeButton />
-</div>
-
-)};
+  return <SwipeButton />;
+};
 
 export default BikeTransfer;
