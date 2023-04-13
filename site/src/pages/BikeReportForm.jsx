b304a537639dc7f13bb2d9bee25bb9cf1603649c
Abdallah Ziad Al Naif <Abdallahalnaif@hotmail.com>
2023-04-13 09:54:12 +0200

    Making the Report stolen bike form

---
+++ site/src/pages/BikeReportForm.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { BikeComponent } from "../components/MyBikes/BikeComponent";
+
+export const BikeReportForm = () => {
+  return (
+    <div className="">
+      <p>hello</p>
+    </div>
+  );
+};
---
+++ site/src/pages/ReportFoundBike.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { BikeComponent } from "../components/MyBikes/BikeComponent";
+
+export const ReportFoundBike = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="">
+      <form>
+        <label
+          for="default-search"
+          className="sr-only mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >
+          Search
+        </label>
+        <div className="relative">
+          <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+            <svg
+              aria-hidden="true"
+              class="h-5 w-5 text-gray-500 dark:text-gray-400"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              ></path>
+            </svg>
+          </div>
+          <input
+            type="search"
+            id="default-search"
+            class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-4 pl-10 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+            placeholder="Search Mockups, Logos..."
+            required
+          />
+          <button
+            type="submit"
+            class="absolute right-2.5 bottom-2.5 rounded-lg bg-blue-700 px-4 py-2 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Search
+          </button>
+        </div>
+      </form>
+      {/* <BikeComponent /> */}
+
+      <button
+        type="button"
+        onClick={navigate("/bikereportform")}
+        class="flex w-full items-center justify-center rounded-lg  bg-blue-600 py-2 px-4 text-center text-base font-semibold text-white shadow-md transition duration-200 ease-in hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2  focus:ring-offset-blue-200 "
+      >
+        Indrapporter
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="h-6 w-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+          />
+        </svg>
+      </button>
+    </div>
+  );
+};
